8c94029acfa9671c7c5ac976fe0bccde923677b8
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-22 18:39:35 +0100

    Add logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
 export { Button } from './components/Button';
+export { Classroom } from './components/Classroom';
+export { Logo } from './components/Logo';
 export { useCounter } from './hooks/useCounter';
