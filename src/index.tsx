85f606548d810028237faec7d8fce22696c314d0
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-22 18:58:42 +0100

    Add themes

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ export { Button } from './components/Button';
 export { Classroom } from './components/Classroom';
 export { Logo } from './components/Logo';
 export { SearchBar } from './components/SearchBar';
-export { useCounter } from './hooks/useCounter';
+export { lightTheme, darkTheme } from './themes';
