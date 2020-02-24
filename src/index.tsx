53cfc87c2cc3812e9dd1cc6ccba2d5e08e09040b
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-24 18:12:43 +0100

    Add AppShell

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,4 @@ export { Classroom } from './components/Classroom';
 export { Logo } from './components/Logo';
 export { SearchBar } from './components/SearchBar';
 export { lightTheme, darkTheme } from './themes';
+export { AppShell } from './AppShell';
