c5c04caddb18d0beccfa4f0d286c26905b0d0cb9
Patryk Niedzwiedzinski <pniedzwiedzinski19@gmail.com>
2020-03-11 22:21:46 +0100

    Export TimetableCell

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ export { Button } from './components/Button';
 export { Classroom } from './components/Classroom';
 export { Logo } from './components/Logo';
 export { SearchBar } from './components/SearchBar';
-
+export { TimetableCell } from './components/TimetableCell';
 export { lightTheme, darkTheme } from './themes';
 export { AppShell } from './AppShell';
